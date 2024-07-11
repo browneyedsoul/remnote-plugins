e60b79933e945431f96a205dfd25ead7311d1eba
Min-Ki Park <contact@minki.pk>
2024-07-12 03:54:44 +0900

    feat: add table type def

---
+++ packages/minimal-table/src/types/table.d.ts
@@ -0,0 +1,10 @@
+type TableHeader = Rem | undefined;
+
+type TableWidth = Rem | undefined;
+
+type TablePowerUpItem = {
+  name: string;
+  code: string;
+  hidden: boolean;
+  onlyProgrammaticModifying: boolean;
+};
